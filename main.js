dfc8aa48978aba521ef1e754039cd52d4eb6edae
Nicolas Bock <nicolasbock@gmail.com>
2019-02-03 08:03:14 -0700

    Add random tilemap
    This change loads a tileset and creates a random tilemap. The level is
    larger than the displayed canvas. The user can use the keyboard to pan
    around in the level.
    
    Signed-off-by: Nicolas Bock <nicolasbock@gmail.com>

--- main.js
+++ main.js
@@ -1,7 +1,69 @@
-var config = {
+const config = {
+  type: Phaser.AUTO,
+  width: 800,
+  height: 600,
   physics: {
     default: "arcade"
+  },
+  scene: {
+    preload: preload,
+    create: create,
+    update: update,
+    render: render
   }
 };
 
-var game = new Phaser.Game(config);
+const game = new Phaser.Game(config);
+
+function preload() {
+  this.load.image("tiles", "assets/tiles.png");
+};
+
+function create() {
+  let level = [];
+  for (let i = 0; i < 60; i++) {
+    level[i] = [];
+    for (let j = 0; j < 60; j++) {
+      level[i][j] = Math.floor(Math.random() * 23) - 1;
+    }
+  }
+
+  const map = this.make.tilemap({
+    data: level,
+    tileWidth: 32,
+    tileHeight: 32
+  });
+
+  const tiles = map.addTilesetImage("tiles");
+  const layer = map.createStaticLayer(0, tiles, 0, 0);
+
+  const camera = this.cameras.main;
+
+  const cursors = this.input.keyboard.createCursorKeys();
+  controls = new Phaser.Cameras.Controls.FixedKeyControl({
+    camera: camera,
+    left: cursors.left,
+    right: cursors.right,
+    up: cursors.up,
+    down: cursors.down,
+    speed: 0.5
+  });
+
+  camera.setBounds(0, 0, map.widthInPixels, map.heightInPixels);
+
+  this.add
+    .text(16, 16, "Arrow keys to scroll", {
+      font: "18px monospace",
+      fill: "#ffffff",
+      padding: { x: 20, y: 10 },
+      backgroundColor: "#000000"
+    })
+    .setScrollFactor(0);
+};
+
+function update(time, delta) {
+  controls.update(delta);
+};
+
+function render() {
+};
