6963ce17b1f792d1409b562dad60be51633cafd0
Nicolas Bock <nicolasbock@gmail.com>
2019-02-03 17:48:57 -0700

    Add Dude and some basic movements
    Signed-off-by: Nicolas Bock <nicolasbock@gmail.com>

--- main.js
+++ main.js
@@ -3,7 +3,12 @@ const config = {
   width: 800,
   height: 600,
   physics: {
-    default: "arcade"
+    default: "arcade",
+    acrcade: {
+      gravity: {
+        y: 300
+      }
+    }
   },
   scene: {
     preload: preload,
@@ -17,52 +22,56 @@ const game = new Phaser.Game(config);
 
 function preload() {
   this.load.image("tiles", "assets/tiles.png");
+  this.load.spritesheet("dude", "assets/dude.png",
+                        { frameWidth: 32,
+                          frameHeight: 32
+                        });
 };
 
 function create() {
+  /* Create empty tile map. */
   let level = [];
-  for (let i = 0; i < 60; i++) {
+  for (let i = 0; i < 30; i++) {
     level[i] = [];
-    for (let j = 0; j < 60; j++) {
-      level[i][j] = Math.floor(Math.random() * 23) - 1;
+    for (let j = 0; j < 30; j++) {
+      level[i][j] = -1;
     }
   }
 
-  const map = this.make.tilemap({
+  /* Create the level layout. */
+  level[3].fill(5, 0, 10);
+
+  this.map = this.make.tilemap({
     data: level,
     tileWidth: 32,
     tileHeight: 32
   });
 
-  const tiles = map.addTilesetImage("tiles");
-  const layer = map.createStaticLayer(0, tiles, 0, 0);
+  const tiles = this.map.addTilesetImage("tiles");
+  this.layer = this.map.createStaticLayer(0, tiles, 0, 0);
+  this.map.setCollisionBetween(0, 22, this.layer);
 
-  const camera = this.cameras.main;
+  this.dude = this.physics.add.sprite(10, 10, "dude");
+  this.dude.setBounce(0.2);
+  this.dude.setGravityY(300);
 
-  const cursors = this.input.keyboard.createCursorKeys();
-  controls = new Phaser.Cameras.Controls.FixedKeyControl({
-    camera: camera,
-    left: cursors.left,
-    right: cursors.right,
-    up: cursors.up,
-    down: cursors.down,
-    speed: 0.5
-  });
-
-  camera.setBounds(0, 0, map.widthInPixels, map.heightInPixels);
-
-  this.add
-    .text(16, 16, "Arrow keys to scroll", {
-      font: "18px monospace",
-      fill: "#ffffff",
-      padding: { x: 20, y: 10 },
-      backgroundColor: "#000000"
-    })
-    .setScrollFactor(0);
+  this.cursors = this.input.keyboard.createCursorKeys();
 };
 
 function update(time, delta) {
-  controls.update(delta);
+  this.physics.collide(this.dude, this.layer);
+
+  if (this.cursors.right.isDown) {
+    this.dude.setVelocityX(100);
+  } else if (this.cursors.left.isDown) {
+    this.dude.setVelocityX(-100);
+  } else {
+    this.dude.setVelocityX(0);
+  }
+
+  if (this.cursors.up.isDown) {
+    this.dude.setVelocityY(-100);
+  }
 };
 
 function render() {
