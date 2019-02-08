be685cf07497ad5b737feb0b4f8a6b7e44d6ec93
Nicolas Bock <nicolasbock@gmail.com>
2019-02-08 16:36:35 -0700

    Add a splash screen with progress bar
    Signed-off-by: Nicolas Bock <nicolasbock@gmail.com>

--- main.js
+++ main.js
@@ -24,6 +24,68 @@ const config = {
 const game = new Phaser.Game(config);
 
 function preload() {
+  let progressBar = this.add.graphics();
+  let progressBox = this.add.graphics();
+  progressBox.fillStyle(0x222222, 0.8);
+  progressBox.fillRect(240, 270, 320, 50);
+
+  let width = this.cameras.main.width;
+  let height = this.cameras.main.height;
+  let loadingText = this.make.text({
+    x: width / 2,
+    y: height / 2 - 50,
+    text: 'Loading...',
+    style: {
+      font: '20px monospace',
+      fill: '#ffffff'
+    }
+  });
+  loadingText.setOrigin(0.5, 0.5);
+
+  let percentText = this.make.text({
+    x: width / 2,
+    y: height / 2 - 5,
+    text: '0%',
+    style: {
+      font: '18px monospace',
+      fill: '#ffffff'
+    }
+  });
+  percentText.setOrigin(0.5, 0.5);
+
+  let assetText = this.make.text({
+    x: width / 2,
+    y: height / 2 + 50,
+    text: '',
+    style: {
+      font: '18px monospace',
+      fill: '#ffffff'
+    }
+  });
+  assetText.setOrigin(0.5, 0.5);
+
+  this.load.on('progress', function (value) {
+    console.log(value);
+    progressBar.clear();
+    progressBar.fillStyle(0xffffff, 1);
+    progressBar.fillRect(250, 280, 300 * value, 30);
+    percentText.setText(parseInt(value * 100) + '%');
+  });
+
+  this.load.on('fileprogress', function (file) {
+    console.log(file.src);
+    assetText.setText('Loading asset: ' + file.key);
+  });
+
+  this.load.on('complete', function () {
+    console.log('complete');
+    progressBar.destroy();
+    progressBox.destroy();
+    loadingText.destroy();
+    percentText.destroy();
+    assetText.destroy();
+  });
+
   this.load.image("tiles", "assets/tiles.png");
   this.load.tilemapTiledJSON("map", "assets/map.json");
   //this.load.tilemapCSV("map", "assets/Maze Runner Levels - Level 1.csv");
