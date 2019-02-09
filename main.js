7fb4d6cfee508cab0acab7e0d3f512e04fa7cbcb
Nicolas Bock <nicolasbock@gmail.com>
2019-02-09 12:31:27 -0700

    Move all function into Phaser.Scene class
    Signed-off-by: Nicolas Bock <nicolasbock@gmail.com>

--- main.js
+++ main.js
@@ -12,11 +12,7 @@ window.onload = function() {
         }
       }
     },
-    scene: {
-      preload: preload,
-      create: create,
-      update: update,
-    },
+    scene: [ playLevel ],
     audio: {
       disableWebAudio: true
     }
@@ -25,222 +21,228 @@ window.onload = function() {
   const game = new Phaser.Game(config);
 };
 
-function preload() {
-  let progressBar = this.add.graphics();
-  let progressBox = this.add.graphics();
-  progressBox.fillStyle(0x222222, 0.8);
-  progressBox.fillRect(240, 270, 320, 50);
-
-  let width = this.cameras.main.width;
-  let height = this.cameras.main.height;
-  let loadingText = this.make.text({
-    x: width / 2,
-    y: height / 2 - 50,
-    text: 'Loading...',
-    style: {
-      font: '20px monospace',
-      fill: '#ffffff'
-    }
-  });
-  loadingText.setOrigin(0.5, 0.5);
-
-  let percentText = this.make.text({
-    x: width / 2,
-    y: height / 2 - 5,
-    text: '0%',
-    style: {
-      font: '18px monospace',
-      fill: '#ffffff'
-    }
-  });
-  percentText.setOrigin(0.5, 0.5);
-
-  let assetText = this.make.text({
-    x: width / 2,
-    y: height / 2 + 50,
-    text: '',
-    style: {
-      font: '18px monospace',
-      fill: '#ffffff'
-    }
-  });
-  assetText.setOrigin(0.5, 0.5);
-
-  this.load.on('progress', function (value) {
-    console.log(value);
-    progressBar.clear();
-    progressBar.fillStyle(0xffffff, 1);
-    progressBar.fillRect(250, 280, 300 * value, 30);
-    percentText.setText(parseInt(value * 100) + '%');
-  });
-
-  this.load.on('fileprogress', function (file) {
-    console.log(file.src);
-    assetText.setText('Loading asset: ' + file.key);
-  });
-
-  this.load.on('complete', function () {
-    console.log('complete');
-    progressBar.destroy();
-    progressBox.destroy();
-    loadingText.destroy();
-    percentText.destroy();
-    assetText.destroy();
-  });
-
-  this.load.image("tiles", "assets/tiles.png");
-  this.load.tilemapTiledJSON("map", "assets/map.json");
-  //this.load.tilemapCSV("map", "assets/Maze Runner Levels - Level 1.csv");
-  //this.load.tilemapCSV("map", "assets/Lucas.csv");
-  this.load.spritesheet("dude", "assets/dude.png",
-                        { frameWidth: 24,
-                          frameHeight: 32
-                        });
-  this.load.spritesheet("heart", "assets/heart.png",
-                        { frameWidth: 10,
-                          frameHeight: 10
-                        });
-  this.load.audio("background_music",
-                  "assets/Ove - Earth Is All We Have .ogg");
-};
-
-function create() {
-  /* Create the map. */
-  this.map = this.make.tilemap({
-    key: "map",
-    tileWidth: 32,
-    tileHeight: 32
-  });
-  this.tiles = this.map.addTilesetImage("tiles");
-  this.layer = this.map.createStaticLayer(0, this.tiles);
-  this.map.setCollisionBetween(0, 22, this.layer);
-
-  /* Resize world to fit the level. */
-  this.physics.world.setBounds(0, 0, this.map.widthInPixels, this.map.heightInPixels);
-
-  /* Add the dude. */
-  this.dude = this.physics.add.sprite(10, 10, "dude");
-  this.dude.setBounce(0.2);
-  this.dude.setGravityY(300);
-  this.dude.setCollideWorldBounds(true);
-
-  this.anims.create({
-    key: "stand",
-    frames: [ { key: "dude", frame: 2 } ]
-  });
-
-  this.anims.create({
-    key: "jump",
-    frames: [ { key: "dude", frame: 0 } ]
-  });
-
-  this.anims.create({
-    key: 'walk',
-    frames: this.anims.generateFrameNumbers("dude", { start: 1, end: 2 }),
-    frameRate: 10,
-    repeat: -1
-  });
-
-  /* This will watch the player and layer every frame to check for
-     collisions. */
-  this.physics.add.collider(this.dude, this.layer);
-
-  /* Create the hearts. */
-  this.anims.create({
-    key: "glimmer",
-    frames: this.anims.generateFrameNumbers("heart"),
-    frameRate: 5,
-    repeat: -1
-  });
-
-  this.hearts = this.physics.add.group({
-    key: "heart",
-    repeat: 10
-  });
-
-  for (let i = 0; i < this.hearts.children.size; i++) {
-    this.hearts.children.entries[i].setBounce(0.2);
-    this.hearts.children.entries[i].setGravityY(100);
-    this.hearts.children.entries[i].setCollideWorldBounds(true);
-    this.hearts.children.entries[i].anims.play("glimmer");
-    this.hearts.children.entries[i].setPosition(200 * i, 10);
+class playLevel extends Phaser.Scene {
+  constructor() {
+    super("PlayLevel");
   }
 
-  this.physics.add.collider(this.hearts, this.layer);
-  this.physics.add.overlap(this.dude, this.hearts, collectHearts, null, this);
+  preload() {
+    let progressBar = this.add.graphics();
+    let progressBox = this.add.graphics();
+    progressBox.fillStyle(0x222222, 0.8);
+    progressBox.fillRect(240, 270, 320, 50);
 
-  this.controls = this.input.keyboard.addKeys({
-    "up": Phaser.Input.Keyboard.KeyCodes.UP,
-    "left": Phaser.Input.Keyboard.KeyCodes.LEFT,
-    "right": Phaser.Input.Keyboard.KeyCodes.RIGHT,
-    "music": Phaser.Input.Keyboard.KeyCodes.M,
-    "debug": Phaser.Input.Keyboard.KeyCodes.D
-  });
+    let width = this.cameras.main.width;
+    let height = this.cameras.main.height;
+    let loadingText = this.make.text({
+      x: width / 2,
+      y: height / 2 - 50,
+      text: 'Loading...',
+      style: {
+        font: '20px monospace',
+        fill: '#ffffff'
+      }
+    });
+    loadingText.setOrigin(0.5, 0.5);
 
-  this.cameras.main.startFollow(this.dude);
-  this.cameras.main.setBounds(0, 0, this.map.widthInPixels, this.map.heightInPixels);
+    let percentText = this.make.text({
+      x: width / 2,
+      y: height / 2 - 5,
+      text: '0%',
+      style: {
+        font: '18px monospace',
+        fill: '#ffffff'
+      }
+    });
+    percentText.setOrigin(0.5, 0.5);
 
-  this.play_music = 0;
-  this.background_music = this.sound.add("background_music", { loop: true });
+    let assetText = this.make.text({
+      x: width / 2,
+      y: height / 2 + 50,
+      text: '',
+      style: {
+        font: '18px monospace',
+        fill: '#ffffff'
+      }
+    });
+    assetText.setOrigin(0.5, 0.5);
 
-  this.heart_points = 0;
-  this.statusText = this.add.text(560, 16, 'Hearts: 0',
-                                  { fontSize: '32px', fill: '#ffffff' });
-};
+    this.load.on('progress', function (value) {
+      console.log(value);
+      progressBar.clear();
+      progressBar.fillStyle(0xffffff, 1);
+      progressBar.fillRect(250, 280, 300 * value, 30);
+      percentText.setText(parseInt(value * 100) + '%');
+    });
 
-function update(time, delta) {
+    this.load.on('fileprogress', function (file) {
+      console.log(file.src);
+      assetText.setText('Loading asset: ' + file.key);
+    });
 
-  if (this.controls.right.isDown) {
-    this.dude.setFlipX(false);
-    this.dude.setVelocityX(100);
-    this.dude.anims.play("walk", true);
-  } else if (this.controls.left.isDown) {
-    this.dude.setFlipX(true);
-    this.dude.setVelocityX(-100);
-    this.dude.anims.play("walk", true);
-  } else {
-    /* Stop any previous movement. */
-    this.dude.setVelocityX(0);
-    this.dude.anims.play("stand");
+    this.load.on('complete', function () {
+      console.log('complete');
+      progressBar.destroy();
+      progressBox.destroy();
+      loadingText.destroy();
+      percentText.destroy();
+      assetText.destroy();
+    });
+
+    this.load.image("tiles", "assets/tiles.png");
+    this.load.tilemapTiledJSON("map", "assets/map.json");
+    //this.load.tilemapCSV("map", "assets/Maze Runner Levels - Level 1.csv");
+    //this.load.tilemapCSV("map", "assets/Lucas.csv");
+    this.load.spritesheet("dude", "assets/dude.png",
+                          { frameWidth: 24,
+                            frameHeight: 32
+                          });
+    this.load.spritesheet("heart", "assets/heart.png",
+                          { frameWidth: 10,
+                            frameHeight: 10
+                          });
+    this.load.audio("background_music",
+                    "assets/Ove - Earth Is All We Have .ogg");
   }
 
-  if (this.controls.up.isDown) {
-    if (this.dude.body.onFloor()) {
-      this.dude.setVelocityY(-130);
-    } else if (this.dude.body.onWall()) {
-      this.dude.setVelocityY(-50);
+  create() {
+    /* Create the map. */
+    this.map = this.make.tilemap({
+      key: "map",
+      tileWidth: 32,
+      tileHeight: 32
+    });
+    this.tiles = this.map.addTilesetImage("tiles");
+    this.layer = this.map.createStaticLayer(0, this.tiles);
+    this.map.setCollisionBetween(0, 22, this.layer);
+
+    /* Resize world to fit the level. */
+    this.physics.world.setBounds(0, 0, this.map.widthInPixels, this.map.heightInPixels);
+
+    /* Add the dude. */
+    this.dude = this.physics.add.sprite(10, 10, "dude");
+    this.dude.setBounce(0.2);
+    this.dude.setGravityY(300);
+    this.dude.setCollideWorldBounds(true);
+
+    this.anims.create({
+      key: "stand",
+      frames: [ { key: "dude", frame: 2 } ]
+    });
+
+    this.anims.create({
+      key: "jump",
+      frames: [ { key: "dude", frame: 0 } ]
+    });
+
+    this.anims.create({
+      key: 'walk',
+      frames: this.anims.generateFrameNumbers("dude", { start: 1, end: 2 }),
+      frameRate: 10,
+      repeat: -1
+    });
+
+    /* This will watch the player and layer every frame to check for
+       collisions. */
+    this.physics.add.collider(this.dude, this.layer);
+
+    /* Create the hearts. */
+    this.anims.create({
+      key: "glimmer",
+      frames: this.anims.generateFrameNumbers("heart"),
+      frameRate: 5,
+      repeat: -1
+    });
+
+    this.hearts = this.physics.add.group({
+      key: "heart",
+      repeat: 10
+    });
+
+    for (let i = 0; i < this.hearts.children.size; i++) {
+      this.hearts.children.entries[i].setBounce(0.2);
+      this.hearts.children.entries[i].setGravityY(100);
+      this.hearts.children.entries[i].setCollideWorldBounds(true);
+      this.hearts.children.entries[i].anims.play("glimmer");
+      this.hearts.children.entries[i].setPosition(200 * i, 10);
     }
+
+    this.physics.add.collider(this.hearts, this.layer);
+    this.physics.add.overlap(this.dude, this.hearts, this.collectHearts, null, this);
+
+    this.controls = this.input.keyboard.addKeys({
+      "up": Phaser.Input.Keyboard.KeyCodes.UP,
+      "left": Phaser.Input.Keyboard.KeyCodes.LEFT,
+      "right": Phaser.Input.Keyboard.KeyCodes.RIGHT,
+      "music": Phaser.Input.Keyboard.KeyCodes.M,
+      "debug": Phaser.Input.Keyboard.KeyCodes.D
+    });
+
+    this.cameras.main.startFollow(this.dude);
+    this.cameras.main.setBounds(0, 0, this.map.widthInPixels, this.map.heightInPixels);
+
+    this.play_music = 0;
+    this.background_music = this.sound.add("background_music", { loop: true });
+
+    this.heart_points = 0;
+    this.statusText = this.add.text(560, 16, 'Hearts: 0',
+                                    { fontSize: '32px', fill: '#ffffff' });
   }
 
-  if (!(this.dude.body.onFloor() || this.dude.body.onWall())) {
-    this.dude.anims.play("jump");
-  }
+  update(time, delta) {
 
-  if (this.controls.music.isDown && (time - this.play_music > 1000)) {
-    if (this.background_music.isPlaying) {
-      this.background_music.stop();
+    if (this.controls.right.isDown) {
+      this.dude.setFlipX(false);
+      this.dude.setVelocityX(100);
+      this.dude.anims.play("walk", true);
+    } else if (this.controls.left.isDown) {
+      this.dude.setFlipX(true);
+      this.dude.setVelocityX(-100);
+      this.dude.anims.play("walk", true);
     } else {
-      this.background_music.play();
+      /* Stop any previous movement. */
+      this.dude.setVelocityX(0);
+      this.dude.anims.play("stand");
     }
-    this.play_music = time;
+
+    if (this.controls.up.isDown) {
+      if (this.dude.body.onFloor()) {
+        this.dude.setVelocityY(-130);
+      } else if (this.dude.body.onWall()) {
+        this.dude.setVelocityY(-50);
+      }
+    }
+
+    if (!(this.dude.body.onFloor() || this.dude.body.onWall())) {
+      this.dude.anims.play("jump");
+    }
+
+    if (this.controls.music.isDown && (time - this.play_music > 1000)) {
+      if (this.background_music.isPlaying) {
+        this.background_music.stop();
+      } else {
+        this.background_music.play();
+      }
+      this.play_music = time;
+    }
+
+    if (this.controls.debug.isDown) {
+      if (!this.debugGraphics) {
+        this.debugGraphics = this.add.graphics().setAlpha(0.75);
+        this.layer.renderDebug(this.debugGraphics, {
+          tileColor: null, // Color of non-colliding tiles
+          collidingTileColor: new Phaser.Display.Color(243, 134, 48, 255), // Color of colliding tiles
+          faceColor: new Phaser.Display.Color(40, 39, 37, 255) // Color of colliding face edges
+        });
+      } else {
+        this.debugGraphics.destroy();
+      }
+    }
   }
 
-  if (this.controls.debug.isDown) {
-    if (!this.debugGraphics) {
-      this.debugGraphics = this.add.graphics().setAlpha(0.75);
-      this.layer.renderDebug(this.debugGraphics, {
-        tileColor: null, // Color of non-colliding tiles
-        collidingTileColor: new Phaser.Display.Color(243, 134, 48, 255), // Color of colliding tiles
-        faceColor: new Phaser.Display.Color(40, 39, 37, 255) // Color of colliding face edges
-      });
-    } else {
-      this.debugGraphics.destroy();
-    }
+  collectHearts(dude, heart) {
+    heart.disableBody(true, true);
+    this.heart_points += 10;
+    this.statusText.setText('Hearts: ' + this.heart_points);
   }
-};
-
-function collectHearts(dude, heart) {
-  heart.disableBody(true, true);
-  this.heart_points += 10;
-  this.statusText.setText('Hearts: ' + this.heart_points);
-};
+}
