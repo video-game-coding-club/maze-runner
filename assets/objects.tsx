c9b45f7a65a9aa7be6378efd7a02ea7e88b27920
Nicolas Bock <nicolasbock@gmail.com>
2019-03-09 06:21:00 -0700

    Convert tilesets to external for level 0
    Also add a script that exports the maps and embeds the tilesets.
    
    Signed-off-by: Nicolas Bock <nicolasbock@gmail.com>

---
+++ assets/objects.tsx
@@ -0,0 +1,22 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset version="1.2" tiledversion="1.2.2" name="objects" tilewidth="24" tileheight="47" tilecount="3" columns="0">
+ <grid orientation="orthogonal" width="1" height="1"/>
+ <tile id="0">
+  <properties>
+   <property name="type" value="heart"/>
+  </properties>
+  <image width="18" height="18" source="heart_green_frame.png"/>
+ </tile>
+ <tile id="1">
+  <properties>
+   <property name="type" value="torch"/>
+  </properties>
+  <image width="16" height="47" source="torch_object.png"/>
+ </tile>
+ <tile id="2">
+  <properties>
+   <property name="type" value="spawn"/>
+  </properties>
+  <image width="24" height="26" source="dude_object.png"/>
+ </tile>
+</tileset>
