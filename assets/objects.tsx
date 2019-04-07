764617e9b53e8f4620f81055a76ccdb4c55be215
Nicolas Bock <nicolasbock@gmail.com>
2019-04-07 06:09:38 -0600

    Add ogre in level 0
    Signed-off-by: Nicolas Bock <nicolasbock@gmail.com>

--- assets/objects.tsx
+++ assets/objects.tsx
@@ -1,5 +1,5 @@
 <?xml version="1.0" encoding="UTF-8"?>
-<tileset version="1.2" tiledversion="1.2.2" name="objects" tilewidth="24" tileheight="47" tilecount="4" columns="0">
+<tileset version="1.2" tiledversion="1.2.2" name="objects" tilewidth="64" tileheight="64" tilecount="5" columns="0">
  <grid orientation="orthogonal" width="1" height="1"/>
  <tile id="0" type="heart">
   <image width="18" height="18" source="heart_green_frame.png"/>
@@ -12,5 +12,8 @@
  </tile>
  <tile id="3" type="gems">
   <image width="16" height="16" source="jewel_green_object.png"/>
+ </tile>
+ <tile id="4" type="enemy">
+  <image width="32" height="32" source="ogre_symbol.png"/>
  </tile>
 </tileset>
